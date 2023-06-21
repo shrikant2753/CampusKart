7eda0f96b544b82a105f35eeba61a5c56266a877
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-21 21:16:20 +0530

    Initial Commit

---
+++ src/main/java/com/example/CampusKart/CampusKartApplication.java
@@ -0,0 +1,13 @@
+package com.example.CampusKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CampusKart/CampusKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CampusKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
