2b09aecf6319105f182d26a582b8250c31f58e02
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 17:16:27 +0530

    branch entity added

--- src/main/java/com/example/campusKart/Entity/Branches.java
+++ src/main/java/com/example/campusKart/Entity/Branches.java
@@ -1,2 +1,16 @@
-package com.example.campusKart.Entity;public class Branches {
+package com.example.campusKart.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "branch")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Branches {
+    private String branch;
 }
