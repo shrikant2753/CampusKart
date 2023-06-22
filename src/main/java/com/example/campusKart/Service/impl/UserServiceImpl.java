65cd21bcc1b4313e133724f4947217f29c1f0077
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 09:56:59 +0530

    password validations added

--- src/main/java/com/example/campusKart/Service/impl/UserServiceImpl.java
+++ src/main/java/com/example/campusKart/Service/impl/UserServiceImpl.java
@@ -8,6 +8,9 @@ import com.example.campusKart.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
@@ -23,7 +26,11 @@ public class UserServiceImpl implements UserService {
 //        if(existedUser!=null){
 //            throw new Exception("Email already exist");
 //        }
-
+        Pattern p = Pattern.compile("((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20})");
+        Matcher m= p.matcher(userEntryDto.getPassword());
+        if(!m.matches()){
+            throw new Exception("Password contain min one small letter, one capital letter, one digit and one spacial character ");
+        }
         User user = UserConverter.convertDtoToEntity(userEntryDto);
         userRepository.save(user);
         return user.toString();
