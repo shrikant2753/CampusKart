a16d43c9ec9055eb815b5befb6960fa6ef204703
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 12:37:37 +0530

    Validations added on email, mobile password

--- src/main/java/com/example/campusKart/Service/UserService.java
+++ src/main/java/com/example/campusKart/Service/UserService.java
@@ -2,6 +2,8 @@ package com.example.campusKart.Service;
 
 import com.example.campusKart.EntryDTOs.UserEntryDto;
 
+import javax.validation.ValidationException;
+
 public interface UserService {
     String addUser(UserEntryDto userEntryDto) throws Exception;
 }
