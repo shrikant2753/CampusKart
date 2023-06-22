e2c7e6d3751e3b59d06f3129eadf20c969f8ed84
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 17:15:34 +0530

    college repository added

--- src/main/java/com/example/campusKart/Repository/CollegeRepository.java
+++ src/main/java/com/example/campusKart/Repository/CollegeRepository.java
@@ -1,2 +1,10 @@
-package com.example.campusKart.Repository;public class CollegeRepository {
+package com.example.campusKart.Repository;
+
+import com.example.campusKart.Entity.College;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CollegeRepository extends MongoRepository<College, ObjectId> {
 }
