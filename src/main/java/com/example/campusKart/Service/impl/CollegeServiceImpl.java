23290ae57a91f3c9d0f4e212ed394a2262f00452
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 18:40:13 +0530

    add collegeServiceImpl

--- src/main/java/com/example/campusKart/Service/impl/CollegeServiceImpl.java
+++ src/main/java/com/example/campusKart/Service/impl/CollegeServiceImpl.java
@@ -1,2 +1,20 @@
-package com.example.campusKart.Service.impl;public class CollegeServiceImpl {
+package com.example.campusKart.Service.impl;
+
+import com.example.campusKart.Entity.College;
+import com.example.campusKart.Repository.CollegeRepository;
+import com.example.campusKart.Service.CollegeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CollegeServiceImpl implements CollegeService {
+    @Autowired
+    CollegeRepository collegeRepository;
+
+    @Override
+    public String addCollege(String collegeName) {
+        College college = new College(collegeName);
+        collegeRepository.save(college);
+        return "College Added Successfully";
+    }
 }
