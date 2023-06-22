192684f0c6a5c15edc1f4ca7d0ed0e4d8fbb87ae
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 18:39:22 +0530

    add collegeController

--- src/main/java/com/example/campusKart/Controller/CollegeController.java
+++ src/main/java/com/example/campusKart/Controller/CollegeController.java
@@ -1,2 +1,20 @@
-package com.example.campusKart.Controller;public class CollegeController {
+package com.example.campusKart.Controller;
+
+import com.example.campusKart.Service.CollegeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/college")
+public class CollegeController {
+    @Autowired
+    CollegeService collegeService;
+
+    @PostMapping("/add")
+    public String addCollege(@RequestParam String collegeName){
+        return collegeService.addCollege(collegeName);
+    }
 }
