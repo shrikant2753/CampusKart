bcdf7cb02528b758ff9e6c7dcbc2423e5ea1176b
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-22 17:17:10 +0530

    branch repository added

--- src/main/java/com/example/campusKart/Repository/BranchRepository.java
+++ src/main/java/com/example/campusKart/Repository/BranchRepository.java
@@ -1,2 +1,10 @@
-package com.example.campusKart.Repository;public interface BranchRepository {
+package com.example.campusKart.Repository;
+
+import com.example.campusKart.Entity.Branches;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BranchRepository extends MongoRepository<Branches, ObjectId> {
 }
