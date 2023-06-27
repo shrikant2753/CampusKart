5b2d23b9ca273292a5dcce539ceeb186adc8522a
shrikant2753 <shrikantrp.00@gmail.com>
2023-06-27 17:32:59 +0530

    product added

--- src/main/java/com/example/campusKart/Product/Entity/Product.java
+++ src/main/java/com/example/campusKart/Product/Entity/Product.java
@@ -1,7 +1,24 @@
-package com.example.campusKart;
+package com.example.campusKart.Product.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collation="product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
+
+    private String type;
+    private int quantity;
+    private double costPrice;
+    private double sellingPrice;
+    private String relDepartment;
+    private int relYear;
+    private int relSemester;
+    private String description;
 }
