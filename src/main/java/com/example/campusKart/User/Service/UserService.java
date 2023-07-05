d9040230f0b97ff3567873c324a2120d9b980b5a
shrikant2753 <shrikantrp.00@gmail.com>
2023-07-05 11:53:16 +0530

    login api

--- src/main/java/com/example/campusKart/User/Service/UserService.java
+++ src/main/java/com/example/campusKart/User/Service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.campusKart.User.Service;
 
 import com.example.campusKart.User.EntryDTOs.UserEntryDto;
+import com.example.campusKart.User.EntryDTOs.UserLoginDto;
+import com.example.campusKart.User.ResponseDTOs.UserLoginResponseDto;
 
 public interface UserService {
     String addUser(UserEntryDto userEntryDto) throws Exception;
+    UserLoginResponseDto login(UserLoginDto userLoginDto) throws Exception;
 }
