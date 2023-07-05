4e2fadb57392a69b4860861774eaf4202976ce58
shrikant2753 <shrikantrp.00@gmail.com>
2023-07-05 11:58:12 +0530

    login response

--- src/main/java/com/example/campusKart/User/Payload/LoginResponse.java
+++ src/main/java/com/example/campusKart/User/Payload/LoginResponse.java
@@ -1,2 +1,12 @@
-package com.example.campusKart.User.Payload;public class LoginException {
+package com.example.campusKart.User.Payload;
+
+import com.example.campusKart.User.ResponseDTOs.UserLoginResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    String message;
+    UserLoginResponseDto userLoginResponseDto;
 }
