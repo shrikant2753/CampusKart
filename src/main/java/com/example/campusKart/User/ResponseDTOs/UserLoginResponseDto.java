0fd5960b5288fecee44bff2e50c37826a6efa12d
shrikant2753 <shrikantrp.00@gmail.com>
2023-07-05 11:57:42 +0530

    response for login

--- src/main/java/com/example/campusKart/User/ResponseDTOs/UserLoginResponseDto.java
+++ src/main/java/com/example/campusKart/User/ResponseDTOs/UserLoginResponseDto.java
@@ -1,2 +1,17 @@
-package com.example.campusKart.User.ResponseDTOs;public class UserLoginResponseDto {
+package com.example.campusKart.User.ResponseDTOs;
+
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+@Data
+@Builder
+public class UserLoginResponseDto {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private String collegeName;
+    private int year;
+    private String branch;
 }
