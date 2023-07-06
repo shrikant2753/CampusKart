65693ecdd27c64cfb00fc491ee33a6efbd405009
shrikant2753 <shrikantrp.00@gmail.com>
2023-07-06 16:39:15 +0530

    product_id and user_id added

--- src/main/java/com/example/campusKart/Product/Entity/Product.java
+++ src/main/java/com/example/campusKart/Product/Entity/Product.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
@@ -15,7 +17,9 @@ import java.util.ArrayList;
 @NoArgsConstructor
 @Builder
 public class Product {
-
+    @Id
+    private ObjectId _id;
+    private ObjectId user_id;
     @NonNull
     private String type;
     @NonNull
